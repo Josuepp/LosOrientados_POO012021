9cccc6d8191becfb563378c4f33ef4c39cd26561
Josuepp <001797920@uca.edu.sv>
2021-03-13 14:31:27 -0600

    End First Program

---
+++ ActOptativa_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ActOptativa_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  a = 0, b = 0;
+            
+            Console.WriteLine("Digite el valor a: ");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el valor de b: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Adding {a} + {b} equals {AddNumbers(a,b)} ");
+        }
+
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
